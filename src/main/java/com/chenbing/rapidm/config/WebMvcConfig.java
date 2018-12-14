514f974b9ece2c55f2e5f357cb948afd0c861ef1
chenbing <443690677@qq.com>
2018-12-14 15:55:27 +0800

    添加外部页面访问功能

--- src/main/java/com/chenbing/rapidm/config/WebMvcConfig.java
+++ src/main/java/com/chenbing/rapidm/config/WebMvcConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/html/**").addResourceLocations("file:C:/Users/chenbing/Desktop/");
